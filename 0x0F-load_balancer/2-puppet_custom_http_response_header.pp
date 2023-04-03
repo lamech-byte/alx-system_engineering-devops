fdbb07c93a3dfe63f9e7ee04a132f59a48a66f3d
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-04-03 20:27:51 +0300

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,1 +1,10 @@
+# Automates the task of creating a custom
+# HTTP header response, but with Puppet.
 
+exec { 'add_header':
+  command     => "/bin/echo 'add_header X-Served-By ${hostname};' >> /etc/nginx/nginx.conf",
+  unless      => "grep -q 'add_header X-Served-By' /etc/nginx/nginx.conf",
+  path        => ['/usr/bin', '/bin'],
+  refreshonly => true,
+  notify      => Service['nginx'],
+}
