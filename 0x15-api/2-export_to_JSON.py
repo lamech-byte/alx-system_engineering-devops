86ecf0f7dd8fb40d80af004ed1a8147b8c17b368
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-04-24 21:33:37 +0300

    Update 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to CSV format."""
-import csv
+"""Exports to-do list information for a given employee ID to JSON format."""
+import json
 import requests
 import sys
 
 if __name__ == "__main__":
-    # Get the employee ID from the command-line arguments
+    
+    # Check if the correct number of arguments is provided
     user_id = sys.argv[1]
     
     # URL of the API endpoint to get the employee information
@@ -17,10 +18,10 @@ if __name__ == "__main__":
     
     # Get the employee's tasks
     todos = requests.get(url + "todos", params={"userId": user_id}).json()
-    
-    # Export the tasks in JSON format
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
-         ) for t in todos]
+
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
