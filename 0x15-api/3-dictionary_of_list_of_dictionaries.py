4833ba70ecc0935972ce9b4fc6ca1ce0a8d875d6
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-04-24 21:29:55 +0300

    Create 3-dictionary_of_list_of_dictionaries.py

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Extend your Python script to export data in the JSON format.
+"""
+import json
+import requests
+
+if __name__ == "__main__":
+    # retrieve all users
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
+    
+    # create a dictionary to group tasks by user ID and export the dictionary to a JSON file
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
