03ee04b7edcb41cfea05120660a89cad0977ff0e
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-05-09 12:34:41 +0300

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,11 +3,23 @@
 import requests
 
 def number_of_subscribers(subreddit):
+    """
+    This function queries the Reddit API and returns the number of subscribers for a given subreddit.
+
+    Args:
+    subreddit: The subreddit name as a string.
+
+    Returns:
+    The number of subscribers as an integer.
+    If an invalid subreddit is given, returns 0.
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    user_agent = "My User Agent"
+    headers = {"User-Agent": user_agent}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
-    else:
+
+    if response.status_code != 200:
         return 0
+    else:
+        data = response.json().get("data")
+        return data.get("subscribers")
