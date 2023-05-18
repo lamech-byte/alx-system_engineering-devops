4094b99287ba548e12e44b3e6fe30a495d397ba9
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-05-18 22:04:31 +0300

    0x1B-web_stack_debugging_4

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
-# Increase hard file limit.
-exec { 'increase-hard-file-limit':
+# Increase hard file limit for Holberton user.
+exec { 'increase-hard-file-limit-for-holberton-user':
   command => 'sed -i "/alx hard/s/5/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
 
-# Increase soft file limit
-exec { 'increase-soft-file-limit':
+# Increase soft file limit for Holberton user
+exec { 'increase-soft-file-limit-for-holberton-user':
   command => 'sed -i "/alx soft/s/4/50000/" /etc/security/limits.conf',
   path    => '/usr/local/bin/:/bin/'
 }
